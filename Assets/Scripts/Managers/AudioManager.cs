97915d92853889a074439a9eeb6c9ccfa9b8b302
Ewen Cossec <34517747+EwenCsc@users.noreply.github.com>
2024-04-14 01:06:20 +0200

    debut de son

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -1,12 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 
 public class AudioManager : Singleton<AudioManager>
 {
+	#region Fields
+	[SerializeField] private AudioSource _soundDesignSource = null;
+	[SerializeField] private MultiAudioClip _fishingRodBad = null;
+	[SerializeField] private MultiAudioClip _fishingRodGood = null;
+	#endregion Fields
+
 	private void Start()
 	{
 		DontDestroyOnLoad(this);
+	}
+
+	private void PlaySound(AudioClip clip)
+	{
+		_soundDesignSource.clip = clip;
+		_soundDesignSource.Play();
 	}
 }
