5012a4be72af802bec048d46685d240515b8ce6b
FloreauLuca <lucafloreau@akeonet.com>
2024-04-14 15:16:01 +0200

    Add end scene

---
+++ Assets/Scripts/Gameplay/EndScene.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class EndScene : MonoBehaviour
+{
+	[SerializeField] private PlayableDirector _playableDirector = null;
+
+	private void Start()
+	{
+		_playableDirector.stopped += OnStopped;
+	}
+
+	private void OnStopped(PlayableDirector director)
+	{
+		GameManager.Instance.Restart();
+	}
+}
